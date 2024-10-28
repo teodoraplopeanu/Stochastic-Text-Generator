3a0d925a4573047ccffc46dc995d7ba13d564e51
Teodora Plopeanu <152170132+teodoraplopeanu@users.noreply.github.com>
2024-10-28 19:39:41 +0200

    Update k_secv_idx.m

--- k_secv_idx.m
+++ k_secv_idx.m
@@ -1,7 +1,5 @@
 function retval = k_secv_idx (distinct_k_sec)
   % return disctionary containing the indices of each elem from distinct_k_sec 
-  % HINT: containers.Map
-  % Try not to set the key-value pairs iteratively, use containers.Map(keys, values) whenever possible
   n = numel(distinct_k_sec);
   retval = containers.Map(distinct_k_sec, 1:n);
 endfunction
