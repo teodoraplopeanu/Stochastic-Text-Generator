b3da3f681da1d86162174d4bf77d3c27ead44104
Teodora Plopeanu <152170132+teodoraplopeanu@users.noreply.github.com>
2024-10-28 19:38:52 +0200

    Update distinct_k_secv.m

--- distinct_k_secv.m
+++ distinct_k_secv.m
@@ -1,8 +1,4 @@
 function unique_cell_array = distinct_k_secv (cell_array)
-  % TODO: Find unique strings HINT: unique
-  % K secquences should come joined with strjoin
-  % this function will look exactly like distinct_words
-
   % Gasesc secventele unice
   unique_cells = unique(cell_array);
   
