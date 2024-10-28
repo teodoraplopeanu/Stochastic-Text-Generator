7c3ab41a61713916900cbda6b2cab4b282d6aed4
Teodora Plopeanu <152170132+teodoraplopeanu@users.noreply.github.com>
2024-10-28 19:41:34 +0200

    Update word_idx.m

--- word_idx.m
+++ word_idx.m
@@ -1,5 +1,5 @@
 function retval = word_idx (distinct_wds)
-    % return the word_idx dictionary HINT: containers.Map
+    % return the word_idx dictionary
     n = numel(distinct_wds);
     retval = containers.Map(distinct_wds, 1:n);
 endfunction
